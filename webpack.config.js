9cc89e6efbc1c7e91198e8385f69e69b5e76a301
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-08-29 18:05:26 +0100

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    filename: '[name][contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'To-Do List',
+      template: 'src/index.html',
+    }),
+  ],
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'docs'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
