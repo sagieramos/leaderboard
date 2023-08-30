3f946871aa88d2cc047886a65e837f829aff2a5f
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-08-30 19:37:27 +0100

    Created Leaderboad class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import './styles/style.css';
-import displayRecentScores from './modules/util';
+import Leaderboard from './modules/util';
 
-document.addEventListener('DOMContentLoaded', () => {
-    displayRecentScores('#scores', 'li');
-})
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+const leaderboard = new Leaderboard(baseUrl);
+
+
+(async () => {
+    try {
+      await leaderboard.addScoreToGame('Stanley', 4);
+      const scores = await leaderboard.getScoresForGame();
+      console.log(scores);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  })();
